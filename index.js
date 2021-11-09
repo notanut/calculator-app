aa1deb98cc337492b79a05048d4697def634a7f2
notanut <natasyajosy@gmail.com>
2021-11-09 11:22:51 +0700

    adding some CSS and JS

---
+++ index.js
@@ -0,0 +1,14 @@
+$(".first").on("click", () => {
+    $("body").removeClass()
+    $("body").toggleClass("first-theme")
+})
+
+$(".second").on("click", () => {
+    $("body").removeClass()
+    $("body").toggleClass("second-theme")
+})
+
+$(".third").on("click", () => {
+    $("body").removeClass()
+    $("body").toggleClass("third-theme")
+})
